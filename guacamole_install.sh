abe3228c94c2f8bfa8406421f895213be41c0c79
IlyaneDelor <56071981+IlyaneDelor@users.noreply.github.com>
2021-11-13 16:38:39 +0100

    Guac_install

---
+++ guacamole_install.sh
@@ -0,0 +1,61 @@
+apt install make -y
+
+apt install -y build-essential libcairo2-dev libturbojpeg0 libpng-dev libtool-bin libossp-uuid-dev libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libpango1.0-dev libssh2-1-dev libvncserver-dev libtelnet-dev libssl-dev libvorbis-dev libwebp-dev libpulse-dev
+wget https://downloads.apache.org/guacamole/1.3.0/source/guacamole-server-1.3.0.tar.gz
+wget https://downloads.apache.org/guacamole/1.3.0/source/guacamole-server-1.3.0.tar.gz
+cd guacamole-server-1.3.0/
+./configure --with-init-dir=/etc/init.d
+make
+make install
+ldconfig
+ystemctl enable guacd
+systemctl start guacd
+systemctl status guacd
+apt install tomcat9 tomcat9-admin tomcat9-common tomcat9-user -y
+systemctl status tomcat9 
+mkdir /etc/guacamole
+wget https://downloads.apache.org/guacamole/1.3.0/binary/guacamole-1.3.0.war -O /etc/guacamole/guacamole.war
+
+
+ln -s /etc/guacamole/guacamole.war /var/lib/tomcat9/webapps/
+systemctl restart tomcat9
+systemctl restart guacd
+mkdir /etc/guacamole/{extensions,lib}
+echo "GUACAMOLE_HOME=/etc/guacamole"  /etc/default/tomcat9
+apt install mariadb-server mariadb-client
+mysql
+CREATE DATABASE guacamole_db;
+CREATE USER 'guacamole_user'@'localhost' IDENTIFIED BY 'P@$sW0rd';
+GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'localhost';
+FLUSH PRIVILEGES;
+quit;
+
+wget http://apache.mirror.digionline.de/guacamole/1.3.0/binary/guacamole-auth-jdbc-1.3.0.tar.gz
+
+tar vfx guacamole-auth-jdbc-1.3.0.tar.gz
+cat guacamole-auth-jdbc-1.3.0/mysql/schema/*.sql |  mysql guacamole_db
+cp guacamole-auth-jdbc-1.3.0/mysql/guacamole-auth-jdbc-mysql-1.3.0.jar /etc/guacamole/extensions/
+
+wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.13.tar.gz
+tar xvzf mysql-connector-java-8.0.13.tar.gz
+
+cp mysql-connector-java-8.0.13/mysql-connector-java-8.0.13.jar /etc/guacamole/lib/
+
+echo "# Hostname and Guacamole server port
+guacd-hostname: localhost
+guacd-port: 4822
+
+# MySQL properties
+mysql-hostname: localhost
+mysql-port: 3306
+mysql-database: guacamole_db
+mysql-username: guacamole_user
+mysql-password: P@$sW0rd 
+" >> /etc/guacamole/guacamole.properties
+
+ln -s /etc/guacamole /usr/share/tomcat9/.guacamole
+
+systemctl restart tomcat9
+
+
+systemctl restart guacd
